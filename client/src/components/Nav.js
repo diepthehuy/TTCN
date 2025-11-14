4ae811359f3f720f700e1c8dd722a101a13051d6
Huy Diep <huy91856@gmail.com>
2025-11-14 22:13:24 +0700

    loli

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FaUser, FaShoppingCart} from 'react-icons/fa'
+import {FaUser, FaShoppingCart,FaSearch} from 'react-icons/fa'
 const Nav = () => {
   return (
     <nav>
       <div><h1><Link>Shop</Link></h1></div>
-      <input type="text" placeholder='Tìm kiếm sản phẩm'/>
+      <div className='search'>
+        <input type="text" placeholder='Tìm kiếm sản phẩm'/>
+        <Link><FaSearch/></Link>
+      </div>
       <div>
-        <Link><FaUser/></Link>
+        <Link ><FaUser/></Link>
         <Link >Đăng nhập /</Link>
         <Link >Đăng ký</Link>
       </div>
